13298cf9c5fcf56827b7b9addba92f7f677d667d
serim53 <tpfla9953@naver.com>
2022-01-12 20:21:56 +0900

    [구현] 럭키 스트레이트

---
+++ Part 02 주요 알고리즘 이론과 실전 문제/Part03/Q07.py
@@ -0,0 +1,19 @@
+# 럭키 스트레이트
+
+n = input()
+
+length = len(n)
+
+sumLeft = 0
+sumRight = 0
+
+for i in range(length // 2):
+    sumLeft += int(n[i])
+
+for i in range(length//2, length):
+    sumRight += int(n[i])
+
+if sumLeft == sumRight:
+    print("LUCKY")
+else:
+    print("READY")
